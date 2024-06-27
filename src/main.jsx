75b2146483599051e2aec00f08804b8c9f027fbb
jeganpetchimuthu <arunjegan1996@gmail.com>
2024-06-27 08:29:49 +0530

    change Index

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+// Frontend Github Url:https://github.com/Jeganpetchimuthu/MyProjectFrontend
+// Frontend deployed Url:https://my-project-frontend-zeta.vercel.app/
+// Backend Github Url:https://github.com/Jeganpetchimuthu/projectBackend
+// Backend deployed Url:https://project-backend-tan.vercel.app
